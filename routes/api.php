2d652b7d5fb2dfc44a9edbd82c739398ea4796d2
Sebastian Obando <sebastiano-10@hotmail.com>
2025-02-28 20:40:43 -0500

    [FEAT] added dialogFlowController

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,19 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\DialogflowController;
+/* use App\Http\Controllers\UserController; */
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user',  function(Request $request){
+/* Route::middleware('auth:sanctum')->get('/user',  function(Request $request){
     return $request->user();
 });
 
 Route::get('/users', [UserController::class, 'users']);
-Route::get('/login', [UserController::class, 'login']);
+Route::get('/login', [UserController::class, 'login']); */
 
 Route::apiResource('category', CategoryController::class);
 Route::apiResource('product', ProductController::class);
+
+Route::post('/webhook', [DialogflowController::class, 'handleRequest']);
